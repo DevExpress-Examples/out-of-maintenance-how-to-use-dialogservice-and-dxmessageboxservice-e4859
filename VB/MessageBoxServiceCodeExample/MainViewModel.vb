bb34295137590904aa265cffdc904b3d2dd7837f
DevExpressExampleBot <fakeEmail>
2022-12-17 14:05:00 +0400

    Source auto update [skip ci]

--- VB/MessageBoxServiceCodeExample/MainViewModel.vb
+++ VB/MessageBoxServiceCodeExample/MainViewModel.vb
@@ -1,63 +1,69 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports MessageBoxServiceCodeExample.ViewModel
 Imports System.Windows.Input
 
 Namespace MessageBoxServiceCodeExample
-	Public Class MainViewModel
-		Inherits ViewModelBase
 
-		Private privateShowMessageBoxCommand As ICommand
-		Public Property ShowMessageBoxCommand() As ICommand
-			Get
-				Return privateShowMessageBoxCommand
-			End Get
-			Private Set(ByVal value As ICommand)
-				privateShowMessageBoxCommand = value
-			End Set
-		End Property
-		Private privateShowDialogCommand As ICommand
-		Public Property ShowDialogCommand() As ICommand
-			Get
-				Return privateShowDialogCommand
-			End Get
-			Private Set(ByVal value As ICommand)
-				privateShowDialogCommand = value
-			End Set
-		End Property
+    Public Class MainViewModel
+        Inherits ViewModelBase
 
-		Private privateDialogViewModel As DialogViewModel
-		Protected Property DialogViewModel() As DialogViewModel
-			Get
-				Return privateDialogViewModel
-			End Get
-			Private Set(ByVal value As DialogViewModel)
-				privateDialogViewModel = value
-			End Set
-		End Property
+        Private _ShowMessageBoxCommand As ICommand, _ShowDialogCommand As ICommand, _DialogViewModel As DialogViewModel
 
-		Protected ReadOnly Property DialogService() As IDialogService
-			Get
-				Return GetService(Of IDialogService)()
-			End Get
-		End Property
-		Protected ReadOnly Property MessageService() As IMessageBoxService
-			Get
-				Return GetService(Of IMessageBoxService)()
-			End Get
-		End Property
-		Public Sub New()
-			ShowMessageBoxCommand = New DelegateCommand(AddressOf OnShowMessageBoxCommandExecute)
-			ShowDialogCommand = New DelegateCommand(AddressOf OnShowDialogCommandExecute)
-			DialogViewModel = New DialogViewModel()
-		End Sub
-		Private Sub OnShowMessageBoxCommandExecute()
-			MessageService.Show("This is a message box", "DXMessageBoxService", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information)
-		End Sub
-		Private Sub OnShowDialogCommandExecute()
-			Dim resCommand As UICommand = DialogService.ShowDialog(DialogViewModel.DialogCommands, "DialogWindow", DialogViewModel)
-			If resCommand IsNot Nothing Then
-				MessageService.Show("Dialog Clicked Button: " & resCommand.Caption)
-			End If
-		End Sub
-	End Class
+        Public Property ShowMessageBoxCommand As ICommand
+            Get
+                Return _ShowMessageBoxCommand
+            End Get
+
+            Private Set(ByVal value As ICommand)
+                _ShowMessageBoxCommand = value
+            End Set
+        End Property
+
+        Public Property ShowDialogCommand As ICommand
+            Get
+                Return _ShowDialogCommand
+            End Get
+
+            Private Set(ByVal value As ICommand)
+                _ShowDialogCommand = value
+            End Set
+        End Property
+
+        Protected Property DialogViewModel As DialogViewModel
+            Get
+                Return _DialogViewModel
+            End Get
+
+            Private Set(ByVal value As DialogViewModel)
+                _DialogViewModel = value
+            End Set
+        End Property
+
+        Protected ReadOnly Property DialogService As IDialogService
+            Get
+                Return GetService(Of IDialogService)()
+            End Get
+        End Property
+
+        Protected ReadOnly Property MessageService As IMessageBoxService
+            Get
+                Return GetService(Of IMessageBoxService)()
+            End Get
+        End Property
+
+        Public Sub New()
+            ShowMessageBoxCommand = New DelegateCommand(AddressOf OnShowMessageBoxCommandExecute)
+            ShowDialogCommand = New DelegateCommand(AddressOf OnShowDialogCommandExecute)
+            DialogViewModel = New DialogViewModel()
+        End Sub
+
+        Private Sub OnShowMessageBoxCommandExecute()
+            MessageService.Show("This is a message box", "DXMessageBoxService", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Information)
+        End Sub
+
+        Private Sub OnShowDialogCommandExecute()
+            Dim resCommand As UICommand = DialogService.ShowDialog(DialogViewModel.DialogCommands, "DialogWindow", DialogViewModel)
+            If resCommand IsNot Nothing Then MessageService.Show("Dialog Clicked Button: " & resCommand.Caption)
+        End Sub
+    End Class
 End Namespace
--- VB/MessageBoxServiceCodeExample/ViewModel/DialogViewModel.vb
+++ VB/MessageBoxServiceCodeExample/ViewModel/DialogViewModel.vb
@@ -1,92 +1,95 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Windows
 
 Namespace MessageBoxServiceCodeExample.ViewModel
-	Public Class DialogViewModel
-		Inherits ViewModelBase
 
-'INSTANT VB NOTE: The field allowCloseDialog was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private allowCloseDialog_Conflict As Boolean = False
-		Public Property AllowCloseDialog() As Boolean
-			Get
-				Return allowCloseDialog_Conflict
-			End Get
-			Set(ByVal value As Boolean)
-				SetProperty(allowCloseDialog_Conflict, value, Function() AllowCloseDialog)
-			End Set
-		End Property
-'INSTANT VB NOTE: The field userName was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private userName_Conflict As String
-		Public Property UserName() As String
-			Get
-				Return userName_Conflict
-			End Get
-			Set(ByVal value As String)
-				SetProperty(userName_Conflict, value, Function() UserName)
-			End Set
-		End Property
+    Public Class DialogViewModel
+        Inherits ViewModelBase
 
-		Private privateDialogCommands As List(Of UICommand)
-		Public Property DialogCommands() As List(Of UICommand)
-			Get
-				Return privateDialogCommands
-			End Get
-			Private Set(ByVal value As List(Of UICommand))
-				privateDialogCommands = value
-			End Set
-		End Property
-		Private privateRegisterUICommand As UICommand
-		Protected Property RegisterUICommand() As UICommand
-			Get
-				Return privateRegisterUICommand
-			End Get
-			Private Set(ByVal value As UICommand)
-				privateRegisterUICommand = value
-			End Set
-		End Property
-		Private privateCancelUICommand As UICommand
-		Protected Property CancelUICommand() As UICommand
-			Get
-				Return privateCancelUICommand
-			End Get
-			Private Set(ByVal value As UICommand)
-				privateCancelUICommand = value
-			End Set
-		End Property
-		Public Sub New()
-			DialogCommands = New List(Of UICommand)()
-			RegisterUICommand = New UICommand() With {
-				.Caption = "Register",
-				.Command = New DelegateCommand(Of CancelEventArgs)(AddressOf OnRegisterCommandExecute, AddressOf OnRegisterCommandCanExecute),
-				.IsDefault = True,
-				.Id = MessageBoxResult.OK
-			}
-			CancelUICommand = New UICommand() With {
-				.Caption = "Cancel",
-				.Command = New DelegateCommand(Of CancelEventArgs)(AddressOf OnCancelCommandExecute, AddressOf OnCancelCommandCanExecute),
-				.IsCancel = True,
-				.Id = MessageBoxResult.Cancel
-			}
-			DialogCommands.Add(RegisterUICommand)
-			DialogCommands.Add(CancelUICommand)
-		End Sub
-		Private Sub OnRegisterCommandExecute(ByVal parameter As CancelEventArgs)
-			If Not AllowCloseDialog Then
-				parameter.Cancel = True
-			End If
-		End Sub
-		Private Function OnRegisterCommandCanExecute(ByVal parameter As CancelEventArgs) As Boolean
-			Return Not String.IsNullOrEmpty(UserName)
-		End Function
-		Private Sub OnCancelCommandExecute(ByVal parameter As CancelEventArgs)
-			If Not AllowCloseDialog Then
-				parameter.Cancel = True
-			End If
-		End Sub
-		Private Function OnCancelCommandCanExecute(ByVal parameter As CancelEventArgs) As Boolean
-			Return True
-		End Function
-	End Class
+        Private _DialogCommands As List(Of DevExpress.Mvvm.UICommand), _RegisterUICommand As UICommand, _CancelUICommand As UICommand
+
+        Private allowCloseDialogField As Boolean = False
+
+        Public Property AllowCloseDialog As Boolean
+            Get
+                Return allowCloseDialogField
+            End Get
+
+            Set(ByVal value As Boolean)
+                SetProperty(allowCloseDialogField, value, Function() AllowCloseDialog)
+            End Set
+        End Property
+
+        Private userNameField As String
+
+        Public Property UserName As String
+            Get
+                Return userNameField
+            End Get
+
+            Set(ByVal value As String)
+                SetProperty(userNameField, value, Function() UserName)
+            End Set
+        End Property
+
+        Public Property DialogCommands As List(Of UICommand)
+            Get
+                Return _DialogCommands
+            End Get
+
+            Private Set(ByVal value As List(Of UICommand))
+                _DialogCommands = value
+            End Set
+        End Property
+
+        Protected Property RegisterUICommand As UICommand
+            Get
+                Return _RegisterUICommand
+            End Get
+
+            Private Set(ByVal value As UICommand)
+                _RegisterUICommand = value
+            End Set
+        End Property
+
+        Protected Property CancelUICommand As UICommand
+            Get
+                Return _CancelUICommand
+            End Get
+
+            Private Set(ByVal value As UICommand)
+                _CancelUICommand = value
+            End Set
+        End Property
+
+        Public Sub New()
+            DialogCommands = New List(Of UICommand)()
+            RegisterUICommand = New UICommand() With {.Caption = "Register", .Command = New DelegateCommand(Of CancelEventArgs)(AddressOf OnRegisterCommandExecute, AddressOf OnRegisterCommandCanExecute), .IsDefault = True, .Id = MessageBoxResult.OK}
+            CancelUICommand = New UICommand() With {.Caption = "Cancel", .Command = New DelegateCommand(Of CancelEventArgs)(AddressOf OnCancelCommandExecute, AddressOf OnCancelCommandCanExecute), .IsCancel = True, .Id = MessageBoxResult.Cancel}
+            DialogCommands.Add(RegisterUICommand)
+            DialogCommands.Add(CancelUICommand)
+        End Sub
+
+        Private Sub OnRegisterCommandExecute(ByVal parameter As CancelEventArgs)
+            If Not AllowCloseDialog Then
+                parameter.Cancel = True
+            End If
+        End Sub
+
+        Private Function OnRegisterCommandCanExecute(ByVal parameter As CancelEventArgs) As Boolean
+            Return Not String.IsNullOrEmpty(UserName)
+        End Function
+
+        Private Sub OnCancelCommandExecute(ByVal parameter As CancelEventArgs)
+            If Not AllowCloseDialog Then
+                parameter.Cancel = True
+            End If
+        End Sub
+
+        Private Function OnCancelCommandCanExecute(ByVal parameter As CancelEventArgs) As Boolean
+            Return True
+        End Function
+    End Class
 End Namespace
