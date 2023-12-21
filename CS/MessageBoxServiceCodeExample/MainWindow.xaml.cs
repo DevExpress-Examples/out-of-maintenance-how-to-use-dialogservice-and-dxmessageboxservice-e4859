9346f06c836c48be65c3aa04eb709f4030e14163
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-12-21 14:28:42 +0400

    Change example status to out of maintenance

--- CS/MessageBoxServiceCodeExample/MainWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace MessageBoxServiceCodeExample {
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
-}
--- CS/MessageBoxServiceCodeExample/View/DialogView.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace MessageBoxServiceCodeExample.View {
-    public partial class DialogView : UserControl {
-        public DialogView() {
-            InitializeComponent();
-        }
-    }
-}
